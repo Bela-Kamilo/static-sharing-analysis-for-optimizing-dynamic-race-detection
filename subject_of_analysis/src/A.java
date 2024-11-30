48d523c4ccc9ddd44646b46eba81d5a4ea1f2fbb
sathon <csd4210@csd.uoc.gr>
2024-11-30 03:03:31 +0200

    started properly constructing constraints and restructuring

--- subject_of_analysis/src/A.java
+++ subject_of_analysis/src/A.java
@@ -1,18 +1,21 @@
 public class A {
 
 	A x, y,z,w;
-	int f;
+	int fld;
 	public int a(int c){
 		A x= new A();
 		A y= new A();
 		A z= new A();
-		this.z= new A();
 		
 		w = ( c==1 )? x : y; 
 	
-		this.f=0;	
-	
+		this.z= f (x,y);	
+		f2(c,c);		
+
 		return c;
 	}
 
+	public void f2(A p1, A p2){};
+	public void f2(int p1, int p2){};
+	public A f(A a1, A a2){return a1;}
 }
