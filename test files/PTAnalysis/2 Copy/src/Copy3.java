17a96c13aa3f3b4eef53f0f92c5561240e18e617
sathon <csd4210@csd.uoc.gr>
2025-03-17 14:12:31 +0200

    fixed copy3

--- test files/PTAnalysis/2 Copy/src/Copy3.java
+++ test files/PTAnalysis/2 Copy/src/Copy3.java
@@ -3,7 +3,7 @@ public class Copy3 {
 	public void a(A d, int c){
     A x = new A();
     A y = new A();
-    A z = c ? x : y;
+    A z = c==1 ? x : y;
     z.use();	//prevent z from splitting
 		          //in jimple
   }
