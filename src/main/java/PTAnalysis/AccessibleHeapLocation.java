c884596c3ff171015114559d22749287c6a9a383
sathon <csd4210@csd.uoc.gr>
2025-06-04 23:12:25 +0300

    added AccessibleHeapLocation.java

---
+++ src/main/java/PTAnalysis/AccessibleHeapLocation.java
@@ -0,0 +1,21 @@
+package PTAnalysis;
+
+import sootup.core.signatures.FieldSignature;
+import util.Tuple;
+
+/**
+ * Represents a heap location that takes part in a memory access, that is a read or a write.
+ * A field of an object instance. ex. 1.SomeField
+ */
+public class AccessibleHeapLocation {
+    private final int memoryLocation;
+    private final FieldSignature field;
+    public AccessibleHeapLocation(int memoryLocation, FieldSignature f){
+        this.memoryLocation=memoryLocation;
+        this.field=f;
+    }
+    @Override
+    public String toString() {
+        return memoryLocation+"."+field.toString();
+    }
+}
