6bb2dc4a248929c28262c16568953efe31d2dded
Bela-Kamilo <csd4210@csd.uoc.gr>
2025-11-07 20:59:15 +0200

    ref : changed conditions

--- test_files/compile_and_mv_all.sh
+++ test_files/compile_and_mv_all.sh
@@ -1,4 +1,26 @@
-if [ "$1" = "all" ] || [ "$1" = "pta" ]; then
+
+all=false
+pta=false
+se=false
+dr=false
+
+if  [ $# -eq 0 ] || [ "$1" = "all" ] ; then 
+all=true 
+fi
+
+if [ "$1" = "pta" ]; then 
+pta=true 
+fi
+
+if [ "$1" = "se" ] || [ "$1" = "sideeffects" ] || ([[ "$1" = "side" ]] && [[ "$2" = "effects" ]]); then 
+se=true 
+fi
+
+if [ "$1" = "all" ] || [ "$1" = "dr" ] || [ "$1" = "datarace" ] || ([[ "$1" = "data" ]] && [[ "$2" = "race" ]]); then 
+dr=true 
+fi
+
+if $all || $pta; then
 	javac -d PTAnalysis/1\ New  PTAnalysis/1\ New/src/*.java	
 	#^ compile all .java files in "1 New/src" and place them to "1 New"
 	javac -d PTAnalysis/2\ Copy  PTAnalysis/2\ Copy/src/*.java
@@ -11,12 +33,12 @@ if [ "$1" = "all" ] || [ "$1" = "pta" ]; then
 	javac -d PTAnalysis/9\ Other  PTAnalysis/9\ Other/src/*.java
 fi
 
-if [ "$1" = "all" ] || [ "$1" = "se" ] || [ "$1" = "sideeffects" ] || ([[ "$1" = "side" ]] && [[ "$2" = "effects" ]]); then
+if $all || $se; then
 	javac -d SideEffects/1\ Write  SideEffects/1\ Write/src/*.java
 	javac -d SideEffects/2\ Read  SideEffects/2\ Read/src/*.java
 	javac -d SideEffects/3\ MethodInvocation  SideEffects/3\ MethodInvocation/src/*.java
 fi
 
-if [ "$1" = "all" ] || [ "$1" = "dr" ] || [ "$1" = "datarace" ] || ([[ "$1" = "data" ]] && [[ "$2" = "race" ]]); then
+if $all || $dr ; then
 	javac -d DataRace/1  DataRace/1/src/*.java
 fi
