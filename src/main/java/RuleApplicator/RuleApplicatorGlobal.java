5d45f3226fb643b7cbb9c65b4b2cee17dfae18fa
Bela-Kamilo <csd4210@csd.uoc.gr>
2026-01-29 21:43:09 +0200

    added MethodInvocation6 test

---
+++ src/main/java/RuleApplicator/RuleApplicatorGlobal.java
@@ -0,0 +1,13 @@
+package RuleApplicator;
+
+import PTAnalysis.ConstraintManager;
+
+public class RuleApplicatorForAbstractMethods {
+    private final ConstraintManager constraintManager;
+
+    public RuleApplicatorForAbstractMethods(ConstraintManager constraintManager){
+        this.constraintManager=constraintManager;
+    }
+
+
+}
