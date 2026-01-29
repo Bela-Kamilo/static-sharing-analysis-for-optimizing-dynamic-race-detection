661643294a1eada03b5773ebb12afd8af8ca8022
Bela-Kamilo <csd4210@csd.uoc.gr>
2026-01-29 22:00:02 +0200

    feat : abs method, ref : constraints

--- src/main/java/PTAnalysis/PointsToAnalysis.java
+++ src/main/java/PTAnalysis/PointsToAnalysis.java
@@ -3,8 +3,8 @@ package PTAnalysis;
 import GenericSolver.GenericConstraint;
 import PTAnalysis.ConstraintSolver.Constraint;
 import PTAnalysis.ConstraintSolver.Solver;
+import RuleApplicator.RuleApplicatorGlobal;
 import RuleApplicator.RuleApplicatorStmtVisitor;
-import sootup.core.frontend.ResolveException;
 import sootup.core.jimple.common.stmt.Stmt;
 import sootup.core.model.SootMethod;
 import sootup.core.signatures.FieldSignature;
@@ -19,6 +19,8 @@ import util.Tuple;
 import java.util.*;
 import java.util.logging.Logger;
 
+import static util.SootUpStuff.gatherImplementationsOf;
+
 /** This class implements a field sensitive Andersen's-like points to analysis
  *  on jimple code. We do not need to deal with dereferencing and such here.
  *  There s a PointsToSet for every reference local, non-void method, method parameter
@@ -27,18 +29,22 @@ import java.util.logging.Logger;
  *  Every statement (and method) is visited once
 */
 public class PointsToAnalysis {
-    RuleApplicatorStmtVisitor ConstraintGenerator;
-    Set <MethodSignature> visitedMethods;
-    View view;
+    private final ConstraintManager constraintManager;
+    private final RuleApplicatorStmtVisitor ConstraintGenerator;
+    private final Set <MethodSignature> visitedMethods;
+    private final HashMap<MethodSignature, Set<SootMethod>> abstractMethods2Implementations;
+    private final View view;
     private final Logger constraintLogger;
     private boolean hasBeenPerformed=false;
     private final String name;
     public PointsToAnalysis(View view, String analysisName){
+        this.constraintManager = new ConstraintManager();
         this.name=analysisName;
         constraintLogger= new LoggerFactory().createLogger("logs/ConstraintGeneration/",name+" ConstraintGeneration");
-        this.ConstraintGenerator = new RuleApplicatorStmtVisitor();
+        this.ConstraintGenerator = new RuleApplicatorStmtVisitor(constraintManager);
         this.visitedMethods = new HashSet<>();
         this.view=view;
+        this.abstractMethods2Implementations = new HashMap<>();
     }
 
     /** performs the analysis on reachable code from entryMethod
@@ -55,7 +61,7 @@ public class PointsToAnalysis {
      */
     public Map<String, PointsToSet> analise(SootMethod entryMethod){
         GenerateConstraints(entryMethod);
-        Solver solver= new Solver(this.ConstraintGenerator.getPTAconstraints(),name);
+        Solver solver= new Solver(constraintManager.getPTAconstraints(),name);
         Map<String,PointsToSet> res= solver.solve();
         hasBeenPerformed=true;
         //SOLVE FOR SIDE EFFECTS
@@ -74,7 +80,7 @@ public class PointsToAnalysis {
         Queue<MethodSignature> everyOtherMethod= new LinkedList<>(ConstraintGenerator.getMethodsInvoked());
         while(!everyOtherMethod.isEmpty()){
             MethodSignature method = everyOtherMethod.remove();
-            //if ( visitedMethods.contains(method)) continue;
+            //if ( method.getClassSmthn.isLibraryClass()) continue;
             if ( Type.isObjectLikeType(method.getDeclClassType()) || visitedMethods.contains(method)) continue;
 
            // Optional<? extends SootMethod> opt = view.getMethod(method);
@@ -86,20 +92,22 @@ public class PointsToAnalysis {
                 continue;
             }
             generateConstraintsForSingleMethod(nextMethod);
-            //generateConstraintsForSingleMethod(opt.get());
             //note every other method to be passed over
             ConstraintGenerator.getMethodsInvoked().stream().
                         filter(m -> !visitedMethods.contains(m)).
                         forEach(everyOtherMethod::add);
-
         }
         constraintLogger.info("FINISHED GENERATING CONSTRAINTS");
         LoggerFactory.closeHandlerls(constraintLogger);
     }
-    /** passes a single method, notes other visited methods */
+    /** passes over a single method, notes other visited methods */
     public void generateConstraintsForSingleMethod(SootMethod method){
-        try {
+        //try {
             constraintLogger.info("+++Visiting " + method + "+++");
+            if(method.isAbstract()){
+                generateConstrantsForAbsMethod(method);
+                return;
+            }
             constraintLogger.info(method.getBody().toString());
             constraintLogger.info("+++++++++++++++");
             ConstraintGenerator.setVisitingMethod(method.getSignature());
@@ -110,51 +118,78 @@ public class PointsToAnalysis {
             PrintConstraintsToLog();
             constraintLogger.info("------------\nMethods invoked:\n" + ConstraintGenerator.getMethodsInvoked());
             constraintLogger.info("------------");
-        }catch (ResolveException e){    //abstract methods throw an exception when we call getBody()
-                                        //and i dont know how to check if a method is concrete
-            if(!e.toString().contains("not concrete")) throw e;
-            visitedMethods.add(method.getSignature());
+        //}catch (ResolveException e){    //abstract methods throw an exception when we call getBody()
+          //  if(! method.isAbstract())
+          //      throw e;
+            //
+            //visitedMethods.add(method.getSignature());
+       // }
+    }
+    //supposing an abstrat method is called, we assume it can be any one of its implementations
+    //so we will visit them all
+    public void generateConstrantsForAbsMethod(SootMethod method){
+        if(method.isConcrete()) return;
+        constraintLogger.info(method +" is abstract, visiting implementations");
+        constraintLogger.info("+++++++++++++++");
+        Set<SootMethod> methodImplementations = gatherImplementationsOf(method, view);
+        int i=1;
+        for(SootMethod implementation : methodImplementations){
+            constraintLogger.info("v       implementation "+ i++ +" of "+method+"    v");
+            //The abstract method rules should apply to all methods in the view
+            //but we only need those in our possible execution path
+           // /*
+            RuleApplicatorGlobal absMethRules = new RuleApplicatorGlobal(constraintManager);
+            //impl(m).this )= m.this
+            absMethRules.abstractThisGlobalRule(method,implementation);
+            //impl(m).params= m.params
+            absMethRules.abstractParamGlobalRule(method,implementation);
+            //m.return )= impl(m).return     <- contravariant like
+            absMethRules.abstractReturnGlobalRule(method,implementation);
+            // */
+            generateConstraintsForSingleMethod(implementation);
         }
+        visitedMethods.add(method.getSignature());
+        return;
     }
 
     public void PrintConstraintsToLog(){
         constraintLogger.info("---------\nPTConstraints:");
         int i=1;
-        for (Constraint c : ConstraintGenerator.getPTAconstraints() )
+        for (Constraint c : constraintManager.getPTAconstraints() )
             constraintLogger.info((i++) + " "+ c);
         constraintLogger.info("---------");
         constraintLogger.info("---------\nSEConstraints:");
         i=1;
-        for (GenericConstraint c : ConstraintGenerator.getSEConstraints() )
+        for (GenericConstraint<?> c : constraintManager.getSEConstraints() )
             constraintLogger.info((i++) + " "+ c);
         constraintLogger.info("---------");
     }
 
-    public Set<GenericConstraint<AccessibleHeapLocation>> getSEConstraints(){return ConstraintGenerator.getSEConstraints();}
+    public Set<GenericConstraint<AccessibleHeapLocation>> getSEConstraints(){return constraintManager.getSEConstraints();}
 
     public Map<MethodSignature , Set<Tuple<PointsToSet, FieldSignature>>> getFieldsRead(){
         if(!hasBeenPerformed) throw new IllegalStateException("analise() need first be called to yield results");
-        return ConstraintGenerator.getFieldsRead();
+        return constraintManager.getFieldsRead();
     }
 
     public Map<MethodSignature , Set<Tuple<PointsToSet, FieldSignature>>> getFieldsWritten(){
         if(!hasBeenPerformed) throw new IllegalStateException("analise() need first be called to yield results");
-       return ConstraintGenerator.getFieldsWritten();
+       return constraintManager.getFieldsWritten();
     }
 
     public Map<MethodSignature,Set<AccessibleHeapLocation>>  getReads(){
         if(!hasBeenPerformed) throw new IllegalStateException("analise() need first be called to yield results");
-        return ConstraintGenerator.getReads();
+        return constraintManager.getReads();
     }
     public Map<MethodSignature,Set<AccessibleHeapLocation>>  getWrites(){
         if(!hasBeenPerformed) throw new IllegalStateException("analise() need first be called to yield results");
-        return ConstraintGenerator.getWrites();
+        return constraintManager.getWrites();
     }
     public Map<MethodSignature , Set<AccessibleHeapLocation>> getReadSets(){
-        return ConstraintGenerator.getReadSets();
+        return constraintManager.getReadSets();
     }
     public Map<MethodSignature , Set<AccessibleHeapLocation>> getWriteSets(){
-        return ConstraintGenerator.getWriteSets();
+        return constraintManager.getWriteSets();
     }
 
     public boolean hasBeenPerformed(){return hasBeenPerformed;}
